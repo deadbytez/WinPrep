813380b9df29ca68828222d5990f124f071b28f8
Feliks <pukhnastyyy@icloud.com>
2025-06-17 11:24:32 +0200

    v1.1 - Support for GAC, better script name

--- WinPrep.cmd
+++ WinPrep.cmd
@@ -1,12 +1,12 @@
 @echo off
-title PostInstall-WindowsLTSC v1.0
+title WinPrep v1.1
 color 4E
 
 :MAINMENU
 cls
-echo / PostInstall-WindowsLTSC v1.0                                     \
-echo / Changes I apply on fresh installs of Windows LTSC in one script. \
-echo / Repo: https://github.com/azhcat/PostInstall-WindowsLTSC          \
+echo / WinPrep v1.1                                                \
+echo / Changes I apply on fresh installs of Windows in one script. \
+echo / Repo: https://github.com/azhcat/WinPrep                     \
 echo.
 echo WARNING
 echo ----------------------------------------------------------
@@ -14,12 +14,12 @@ echo Review the script's code before running.
 echo.
 echo MAIN MENU
 echo ----------------------------------------------------------
-echo [1] Apply changes
+echo [1] Begin
 echo [2] Exit
 echo.
 set /p mainchoice=Select an option [1-2]: 
 
-if "%mainchoice%"=="1" goto APPLY
+if "%mainchoice%"=="1" goto EDITION
 if "%mainchoice%"=="2" exit /b
 
 echo.
@@ -27,7 +27,30 @@ echo Invalid choice. Please enter 1 or 2.
 pause
 goto MAINMENU
 
+:EDITION
+cls
+echo Select your Windows edition
+echo ----------------------------------------------------------
+echo [1] GAC (Home, Pro, Enterprise...)
+echo [2] LTSC
+echo.
+set /p mainchoice=Select an option [1-2]: 
+
+if "%mainchoice%"=="1" (
+    set EDITION=1
+    goto APPLY
+) else if "%mainchoice%"=="2" (
+    set EDITION=2
+    goto APPLY
+) else (
+    echo.
+    echo Invalid choice. Please enter 1 or 2.
+    pause
+    goto EDITION
+)
+
 :APPLY
+REM These apply both to LTSC and GAC
 reg add "HKCU\Control Panel\Desktop" /v JPEGImportQuality /t REG_DWORD /d 100 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v VerboseStatus /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d 1 /f
@@ -62,6 +85,35 @@ reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\SearchSettings" /v SafeS
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v HideRecentlyAddedApps /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Start_TrackDocs /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SystemSettings\AccountNotifications" /v EnableAccountNotifications /t REG_DWORD /d 0 /f
+reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableSpotlightCollectionOnDesktop /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableSoftLanding /t REG_DWORD /d 1 /f
+
+REM General Availability Channnel entries
+if "%EDITION%"=="1" (
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v ContentDeliveryAllowed /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContentEnabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v OemPreInstalledAppsEnabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v PreInstalledAppsEnabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v PreInstalledAppsEverEnabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SilentInstalledAppsEnabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-310093Enabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338393Enabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-353694Enabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-353696Enabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SystemPaneSuggestionsEnabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338387Enabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v RotatingLockScreenOverlayEnabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338388Enabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338389Enabled /t REG_DWORD /d 0 /f
+    reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SoftLandingEnabled /t REG_DWORD /d 0 /f
+	reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightFeatures /t REG_DWORD /d 1 /f
+	reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightWindowsWelcomeExperience /t REG_DWORD /d 1 /f
+	reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightOnActionCenter /t REG_DWORD /d 1 /f
+	reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsSpotlightOnSettings /t REG_DWORD /d 1 /f
+	reg add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableThirdPartySuggestions /t REG_DWORD /d 1 /f
+)
+
 goto CUTEDGE
 :CUTEDGE
 echo.
@@ -71,12 +123,15 @@ if /I "%cutedge%"=="Y" (
     powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/azhcat/CutEdge/refs/heads/main/CutEdge.cmd' -OutFile '%TEMP%\CutEdge.cmd'"
     call "%TEMP%\CutEdge.cmd"
 ) else if /I "%cutedge%"=="N" (
-    goto :eof
+	echo.
+	echo Done! Please restart your computer.
+	pause
+    goto MAINMENU
 ) else (
     goto CUTEDGE
 )
 cls
-title PostInstall-WindowsLTSC v1.0
+title WinPrep v1.1
 color 4E
 echo. Done! Please restart your computer.
 pause
